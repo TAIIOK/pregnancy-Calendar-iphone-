b977c25df91ff371c5b05b526012ad018965e7e9
Alexander Basov <farestz@iMac-EORU05.local>
2016-03-22 17:38:45 +0300

    - add graph in weightgraph without layouting

--- PregnancyCalendar/PregnancyCalendar/WeightGraphViewController.swift
+++ PregnancyCalendar/PregnancyCalendar/WeightGraphViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class WeightGraphViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, LineChartDelegate {
     
+    // инднекс массы тела
     let IMT0: [CGFloat] = [0.5,0.9,1.4,1.6,1.8,2.0,2.7,3.2,4.5,5.4,6.8,7.7,8.6,9.8,10.2,11.3,12.5,13.6,14.5,15.2]
     let IMT1: [CGFloat] = [0.5,0.7,1.0,1.2,1.3,1.5,1.9,2.3,3.6,4.8,5.7,6.4,7.7,8.2,9.1,10.0,10.9,11.9,12.7,13.6]
     let IMT2: [CGFloat] = [0.5,0.5,0.6,0.7,0.8,0.9,1.0,1.4,2.3,2.9,3.4,3.9,5.0,5.4,5.9,6.4,7.3,7.9,8.6,9.1]
     
+    var mass = 60
     var growth = 0
     var firstComponent = [0, 1, 2]
     var secondComponent = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -29,7 +31,8 @@ class WeightGraphViewController: UIViewController, UIPickerViewDataSource, UIPic
     @IBOutlet weak var label: UILabel!
     
     @IBAction func growthButtonPress(sender: UIBarButtonItem) {
-        self.pickerViewTextField.becomeFirstResponder()
+        self.pickerViewTextField.becomeFirstResponder() // открывает пикер роста
+        self.setupPickerViewValues()
     }
     
     override func viewDidLoad() {
@@ -43,7 +46,7 @@ class WeightGraphViewController: UIViewController, UIPickerViewDataSource, UIPic
     }
     
     private func setupGraph() {
-        if self.growth > 0 {
+        if self.growth != 0 {
             self.drawGraph()
             self.label.hidden = true
         } else {
@@ -73,7 +76,6 @@ class WeightGraphViewController: UIViewController, UIPickerViewDataSource, UIPic
         self.pickerView.dataSource = self
         self.pickerView.backgroundColor = .whiteColor()
         self.pickerViewTextField.inputView = self.pickerView
-        self.setupPickerViewValues()
     }
     
     private func setupPickerViewValues() {
@@ -105,7 +107,85 @@ class WeightGraphViewController: UIViewController, UIPickerViewDataSource, UIPic
     }
     
     func drawGraph() {
+        let factMass = UILabel()
+        factMass.text = "                  Фактический вес"
+        factMass.translatesAutoresizingMaskIntoConstraints = false
+        factMass.textAlignment = NSTextAlignment.Left
+        factMass.textColor=StrawBerryColor
+        self.view.addSubview(factMass)
+        self.views["factMass"] = factMass
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[factMass]-|", options: [], metrics: nil, views: self.views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-100-[factMass]", options: [], metrics: nil, views: self.views))
         
+        let recommend = UILabel()
+        recommend.text = "                 Условно-рекомендуемая норма"
+        recommend.translatesAutoresizingMaskIntoConstraints = false
+        recommend.textAlignment = NSTextAlignment.Center
+        recommend.textColor=UIColor.blueColor()
+        self.view.addSubview(recommend)
+        self.views["recommend"] = recommend
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-100-[recommend]-|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-100-[recommend]", options: [], metrics: nil, views: views))
+        
+        // simple arrays
+        let ves = Double(mass)
+        let rost = Double(growth)
+        let a = CGFloat(51)
+        
+        var data: [CGFloat] = [51]
+        
+        let b = CGFloat(ves)
+        var data1 : [CGFloat] = [b]
+        
+        var imt=ves*10000.0
+        let x: Double = rost*rost
+        if(x>0){
+            imt = imt/x}
+        else{
+            imt = 0
+        }
+        
+        for (var i=0; i<20;i++){
+            if(mass==0){
+                data1.append(0)
+            }
+            else{
+                if(imt < 18.5){
+                    data1.append(b+IMT0[i])
+                }
+                else if (imt >= 25){
+                    data1.append(b+IMT2[i])
+                }
+                else{
+                    data1.append(b+IMT1[i])
+                }
+            }
+            data.append(a+IMT2[i])
+        }
+        
+        // simple line with custom x axis labels
+        let xLabels: [String] = ["0","2", "4", "6", "8", "10", "12", "14", "16", "18", "20", "22", "24", "26", "28", "30", "32", "34", "36", "38", "40"]
+        
+        lineChart = LineChart()
+        lineChart.animation.enabled = true
+        lineChart.area = false
+        lineChart.x.labels.visible = true
+        lineChart.y.labels.visible = true
+        lineChart.x.grid.count = 21
+        lineChart.y.grid.count = 21
+        lineChart.x.labels.values = xLabels
+        lineChart.x.axis.visible = true
+        lineChart.x.axis.color = UIColor.blueColor()
+        lineChart.addLine(data)
+        lineChart.addLine(data1)
+        
+        lineChart.translatesAutoresizingMaskIntoConstraints = false
+        lineChart.delegate = self
+        
+        self.view.addSubview(lineChart)
+        views["chart"] = lineChart
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[chart]-|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[factMass]-[chart(==500)]", options: [], metrics: nil, views: views))
     }
     
     override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
@@ -122,8 +202,9 @@ class WeightGraphViewController: UIViewController, UIPickerViewDataSource, UIPic
         self.setupPickerViewValues()
         
         if self.growth != 0 {
-            self.alertToNotes()
             self.label.hidden = true
+            self.setupGraph()
+            self.alertToNotes()
         } else {
             self.label.hidden = false
         }
